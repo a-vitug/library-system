b516bf6f7e313c4aea0d433d16d4cef456f3e6d9
a-vitug <vtg.allyson@gmail.com>
2026-04-14 21:01:42 -0700

    fixed pages connection

--- source/server/routes.js
+++ source/server/routes.js
@@ -3,6 +3,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/User');
 
+const PAGES_DIR = path.join(__dirname, '../../front-end/pages');
+
+// Home
+router.get('/home', (req, res) => {
+  res.sendFile(path.join(process.cwd(), '../../front-end/pages/home.html'));
+});
+
 // Login
 router.get('/log-in', (req, res) => {
   res.sendFile(path.join(process.cwd(), '../../front-end/pages/authentication/log-in.html'));
@@ -32,12 +39,12 @@ router.post("/log-in", async (req, res) => {
 
 // Create account
 router.get('/create-account', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '../../front-end/pages/authentication/create-account.html'));
+  res.sendFile(path.join(__dirname, '../../front-end/pages/authentication/create-account.html'));
 });
 
 // Genres
 router.get('/genre/:name', (req, res) => {
-  const filePath = path.join(process.cwd(), `../../front-end/pages/genres/${req.params.name}.html`);
+  const filePath = path.join(PAGES_DIR, `../../front-end/pages/genres/${req.params.name}.html`);
   res.sendFile(filePath, (err) => {
     if (err) res.status(404).send('Genre not found');
   });
